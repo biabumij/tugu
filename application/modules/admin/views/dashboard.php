5d445c0e2caf350498f68831827e38a722241fd8
ginanjarbayu92@gmail.com <ginanjarbayu92@gmail.com>
2024-08-23 11:52:15 +0700

    update

--- application/modules/admin/views/dashboard.php
+++ application/modules/admin/views/dashboard.php
@@ -512,7 +512,7 @@
                             ?>
                         </div>
                     </div>
-                    <br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+                    <br /><br /><br /><br /><br />
                 </div>
             </div>
         </div>
