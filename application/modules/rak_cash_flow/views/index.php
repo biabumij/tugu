546fa2588fd04f39e358f3e8f7345304da408ac7
ginanjarbayu92@gmail.com <ginanjarbayu92@gmail.com>
2024-09-18 12:04:20 +0700

    update

--- application/modules/rak_cash_flow/views/index.php
+++ application/modules/rak_cash_flow/views/index.php
@@ -3,45 +3,60 @@
 
 <head>
     <?php echo $this->Templates->Header(); ?>
-    <style type="text/css">
+    <style>
         body {
             font-family: helvetica;
         }
-        
-        .tab-pane {
-            padding-top: 20px;
-        }
 
-        .select2-container--default .select2-results__option[aria-disabled=true] {
-            display: none;
-        }
+        button {
+			border: none;
+			border-radius: 5px;
+			padding: 5px;
+			font-size: 12px;
+			text-transform: uppercase;
+			cursor: pointer;
+			color: white;
+			background-color: #2196f3;
+			box-shadow: 0 0 4px #999;
+			outline: none;
+		}
+
+		.ripple {
+			background-position: center;
+			transition: background 0.8s;
+		}
+		.ripple:hover {
+			background: #47a7f5 radial-gradient(circle, transparent 1%, #47a7f5 1%) center/15000%;
+		}
+		.ripple:active {
+			background-color: #6eb9f7;
+			background-size: 100%;
+			transition: background 0s;
+		}
     </style>
 </head>
 <body>
     <div class="wrap">
-
-        <?php echo $this->Templates->PageHeader(); ?>
-
+        <?php echo $this->Templates->PageHeader();?>
         <div class="page-body">
-            <div class="content">
-                <div class="row animated fadeInUp">
-                    <div class="col-sm-12 col-lg-12">
-                        <div class="panel">
-                            <div class="panel-header">
+                <div class="content">
+                    <div class="row animated fadeInUp">
+                        <div class="col-sm-12 col-lg-12">
+                            <div class="panel">
+                                <div class="panel-header">
                                 <h3><b style="text-transform:uppercase;"><?php echo $row[0]->menu_name; ?></b></h3>
                                 <div class="text-left">
                                     <a href="<?php echo site_url('admin');?>">
-                                    <button style="color:white; background-color:#5bc0de; border:1px solid black; border-radius:10px; line-height:30px;"><b><i class="fa-solid fa-rotate-left"></i> KEMBALI</b></button></a>
+                                    <button class="ripple"><b><i class="fa-solid fa-rotate-left"></i> KEMBALI</b></button></a>
                                 </div>
                             </div>
                             <div class="panel-content">
                                 <ul class="nav nav-tabs" role="tablist">
                                     <li role="presentation" class="active"><a href="#rencana_cash_flow" aria-controls="rencana_cash_flow" role="tab" data-toggle="tab" style="border-radius:10px; font-weight:bold;">RENCANA CASH FLOW</a></li>
                                 </ul>
-
                                 <div class="tab-content">
-								
                                     <div role="tabpanel" class="tab-pane active" id="rencana_cash_flow">
+                                        <br />
                                         <button style="background-color:#88b93c; border:1px solid black; border-radius:10px; line-height:30px;"><a href="<?php echo site_url('rak/form_rencana_cash_flow'); ?>"><b style="color:white;">BUAT RENCANA CASH FLOW</b></a></button>								
                                         <div class="table-responsive">
                                             <table class="table table-striped table-hover" id="table_rencana_cash_flow" style="width:100%">
