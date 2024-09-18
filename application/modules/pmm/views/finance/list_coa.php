643b2115bb81705479c132de03d7e3494f9f2219
ginanjarbayu92@gmail.com <ginanjarbayu92@gmail.com>
2024-09-18 12:28:19 +0700

    update

--- application/modules/pmm/views/finance/list_coa.php
+++ application/modules/pmm/views/finance/list_coa.php
@@ -102,7 +102,7 @@
                                         </select>
                                     </div>
                                     <div class="col-sm-2">
-                                        <button type="submit" class="btn btn-info" style="border-radius:10px;"><i class="fa fa-print"></i> <b>Print</b></button>
+                                        <button type="submit" class="btn btn-default" style="border-radius:10px;"><i class="fa fa-print"></i> <b>Print</b></button>
                                     </div>
                                     <br /><br />
                                 </form>
