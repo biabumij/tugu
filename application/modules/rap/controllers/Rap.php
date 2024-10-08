5310411b4a1cfaafdbe4f36a4fcde71c71760ccf
ginanjarbayu92@gmail.com <ginanjarbayu92@gmail.com>
2024-10-08 13:23:55 +0700

    update

--- application/modules/rap/controllers/Rap.php
+++ application/modules/rap/controllers/Rap.php
@@ -988,6 +988,8 @@ class Rap extends Secure_Controller {
 		$pdf = new Pdf('P', 'mm', 'A4', true, 'UTF-8', false);
         $pdf->setPrintHeader(false);
 		$pdf->setPrintFooter(false);
+		$pdf->SetY(5);
+		$pdf->SetX(5);
         $pdf->SetFont('helvetica','',7); 
         $tagvs = array('div' => array(0 => array('h' => 0, 'n' => 0), 1 => array('h' => 0, 'n'=> 0)));
 		$pdf->setHtmlVSpace($tagvs);
