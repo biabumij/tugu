0acacac6e161c3ba7af9332c961f615b8f13b02c
ginanjarbayu92@gmail.com <ginanjarbayu92@gmail.com>
2024-10-14 11:38:17 +0700

    update

--- application/modules/pmm/models/Pmm_model.php
+++ application/modules/pmm/models/Pmm_model.php
@@ -1603,6 +1603,8 @@ class Pmm_model extends CI_Model {
             $this->db->where('date_po  >=',date('Y-m-d',strtotime($start_date)));   
             $this->db->where('date_po <=',date('Y-m-d',strtotime($end_date)));  
         }
+
+        $this->db->where("status <> 'REJECTED'");
 		$this->db->order_by('created_on','DESC');
         $query = $this->db->get('pmm_purchase_order');
         if($query->num_rows() > 0){
