4b244e70cc11f3792ac0fc85e573d78344b7f552
ginanjarbayu92@gmail.com <ginanjarbayu92@gmail.com>
2024-10-14 11:45:17 +0700

    update

--- application/modules/pembelian/controllers/Pembelian.php
+++ application/modules/pembelian/controllers/Pembelian.php
@@ -1724,7 +1724,14 @@ class Pembelian extends Secure_Controller
 	{
 		$this->db->set("status", "REJECT");
 		$this->db->where("id", $id);
-        $this->db->update('pmm_purchase_order', array('status' => 'REJECT'), array('id' => $id));
+        $this->db->update('pmm_purchase_order', array('status' => 'REJECTED'), array('id' => $id));
+
+        $permintaan_bahan_alat = $this->db->select('(request_material_id) as id')
+        ->from('pmm_purchase_order')
+        ->where('id', $id)
+        ->get()->row_array();
+        $permintaan_bahan_alat_id = $permintaan_bahan_alat['id'];
+        $this->db->update('pmm_request_materials', array('status' => 'REJECTED'), array('id' => $permintaan_bahan_alat_id));
 		$this->session->set_flashdata('notif_reject','<b>REJECTED</b>');
 		redirect("admin/pembelian");
 	}
