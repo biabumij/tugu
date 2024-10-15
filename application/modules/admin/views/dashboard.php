7ee30fec68d89e38c597107b1acfeac2c4b52402
ginanjarbayu92@gmail.com <ginanjarbayu92@gmail.com>
2024-10-15 13:06:50 +0700

    update

--- application/modules/admin/views/dashboard.php
+++ application/modules/admin/views/dashboard.php
@@ -144,7 +144,7 @@
             <?php echo $this->Templates->PageHeader();?>
             <div class="page-body">
                 <div id="about" class="container spacer about">
-                    <div class="col-sm-12" style="background:#fff; font-size:18px; border-radius: 10px; padding:10px; margin-bottom:50px;">
+                    <div class="col-sm-12" style="background-image:linear-gradient(to right,#999999 0%,#333333 51%,#666666 100%); font-size:18px; border-radius: 10px; padding:10px; margin-bottom:50px;">
                         <p style="text-align:center; font-weight:bold; color:white;">PROYEK BENDUNGAN TUGU</p>
                         <?php
                         if(in_array($this->session->userdata('admin_group_id'), array(1,2,3))){
