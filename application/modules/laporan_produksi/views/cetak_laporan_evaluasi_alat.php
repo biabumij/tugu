2f2aca5b4e7776ac085cd8f707d780ebe0fe1067
Bayu <ginanjar.bayubimantoro@biabumijayendra.com>
2024-12-13 14:52:48 +0700

    update

--- application/modules/laporan_produksi/views/cetak_laporan_evaluasi_alat.php
+++ application/modules/laporan_produksi/views/cetak_laporan_evaluasi_alat.php
@@ -81,10 +81,9 @@
 
 	</head>
 	<body>
-		<div align="center" style="display: block;font-weight: bold;font-size: 11px;">EVALUASI BIAYA PERALATAN</div>
-		<div align="center" style="display: block;font-weight: bold;font-size: 11px;">PROYEK BENDUNGAN TUGU</div>
-		<div align="center" style="display: block;font-weight: bold;font-size: 11px;">PT. BIA BUMI JAYENDRA</div>
-		<div align="center" style="display: block;font-weight: bold;font-size: 11px; text-transform: uppercase;">PERIODE : <?php echo str_replace($search, $replace, $subject);?></div>
+		<div align="center" style="display: block;font-weight: bold;font-size: 11px;">Evaluasi Biaya Peralatan</div>
+		<div align="center" style="display: block;font-weight: bold;font-size: 11px;">Proyek Bendungan Tugu</div>
+		<div align="center" style="display: block;font-weight: bold;font-size: 11px;">Periode <?php echo str_replace($search, $replace, $subject);?></div>
 		<br /><br /><br />
 		<?php
 		$data = array();
